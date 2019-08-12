d283168c5e8eb52bbabf7bec6cbf935262508cee
ihahachi <iha2007hachi@gmail.com>
2019-08-12 12:28:04 +0100

    Add reference .Net Sockets

--- WLAN Tools/WLAN Tools/Program.cs
+++ WLAN Tools/WLAN Tools/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace WLAN_Tools
 {
