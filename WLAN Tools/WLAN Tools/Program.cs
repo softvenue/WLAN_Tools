30bfde107b1a3d884092e64adcc1eafa6085329b
ihahachi <iha2007hachi@gmail.com>
2019-08-12 12:34:41 +0100

    Add function GetMacAddress

--- WLAN Tools/WLAN Tools/Program.cs
+++ WLAN Tools/WLAN Tools/Program.cs
@@ -33,5 +33,32 @@ namespace WLAN_Tools
 
             return null;
         }
+
+        public string GetMacAddress(string ipAddress)
+        {
+            string macAddress = string.Empty;
+            System.Diagnostics.Process Process = new System.Diagnostics.Process();
+            Process.StartInfo.FileName = "arp";
+            Process.StartInfo.Arguments = "-a " + ipAddress;
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.RedirectStandardOutput = true;
+            Process.StartInfo.CreateNoWindow = true;
+            Process.Start();
+            string strOutput = Process.StandardOutput.ReadToEnd();
+            string[] substrings = strOutput.Split('-');
+            if (substrings.Length >= 8)
+            {
+                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2))
+                         + "-" + substrings[4] + "-" + substrings[5] + "-" + substrings[6]
+                         + "-" + substrings[7] + "-"
+                         + substrings[8].Substring(0, 2);
+                return macAddress;
+            }
+
+            else
+            {
+                return "OWN Machine";
+            }
+        }
     }
 }
